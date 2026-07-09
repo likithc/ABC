a8f28c795538dbf10f8bd0c5972255349c8ea58f
Daya9096 <141108907+Daya9096@users.noreply.github.com>
2026-07-09 14:43:07 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,19 +1,15 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
 public class App {
 
     private static final String MESSAGE = "Hello World!";
 
-    public App() {}
+    public static void main(String[] args) throws Exception {
 
-    public static void main(String[] args) {
         System.out.println(MESSAGE);
-    }
 
-    public String getMessage() {
-        return MESSAGE;
+        while (true) {
+            Thread.sleep(60000);
+        }
     }
 }
